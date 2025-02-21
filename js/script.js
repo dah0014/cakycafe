a9cbb16ee67c59f268c4736927550287ab610df1
dah0014 <dah0014@naver.com>
2025-02-21 22:59:06 +0900

    caky cafe push

--- js/script.js
+++
@@ -1,8 +0,0 @@
-/*  top button */
-let Top = document.querySelector('.btn-top');
-
-Top.addEventListener('click', (e)=>{
-  e.preventDefault();
-  window.scrollTo({ top: 0, behavior: 'smooth'});
-})
-
